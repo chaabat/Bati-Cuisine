dd4d60d3b37fe2e2992f6b38b96ae368e477a4f9
chaabat <chaabat.02@gmail.com>
2024-09-18 11:22:26 +0100

    DataBaseConnection

--- src/com/BatiCuisine/util/DataBaseConnection.java
+++ src/com/BatiCuisine/util/DataBaseConnection.java
@@ -1,4 +1,35 @@
 package com.BatiCuisine.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
+    private static DataBaseConnection instance ;
+    private Connection connection;
+    private String url = "jdbc:postgresql://localhost:5432/Bati-Cuisine";
+    private String username = "postgres";
+    private String password = "chaabat";
+
+ private DataBaseConnection() throws SQLException{
+     try{
+         Class.forName("org.postgresql.Driver");
+         this.connection = DriverManager.getConnection((url,username,password));
+     }catch (ClassNotFoundException ex){
+         throw new SQLException("PostgreSQL Driver not found");
+     }
+ }
+
+ public Connection getConnection(){
+     return connection;
+ }
+
+ public static  DataBaseConnection getInstance() throws SQLException{
+     if(instance == null){
+         instance = new DataBaseConnection();
+     }else if (instance.getConnection().isClosed()){
+         instance = new DataBaseConnection();
+     }
+     return instance;
+ }
 }
