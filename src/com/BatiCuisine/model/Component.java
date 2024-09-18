1f199883f51ccb9f7c1ffcf75ef7b4cbcebc5af5
chaabat <chaabat.02@gmail.com>
2024-09-18 12:11:18 +0100

    component model

--- src/com/BatiCuisine/model/Component.java
+++ src/com/BatiCuisine/model/Component.java
@@ -1,4 +1,82 @@
 package com.BatiCuisine.model;
 
 public class Component {
+    private int id;
+    private String name;
+    private double unitCost;
+    private double quantity;
+    private String componentType;
+    private double taxRate;
+    private Project project;
+
+// Constructor
+
+    public Component(int id, String name, double unitCost, double quantity, String componentType, double taxRate, Project project) {
+        this.id = id;
+        this.name = name;
+        this.unitCost = unitCost;
+        this.quantity = quantity;
+        this.componentType = componentType;
+        this.taxRate = taxRate;
+        this.project = project;
+    }
+
+    // Getters & Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(double unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getComponentType() {
+        return componentType;
+    }
+
+    public void setComponentType(String componentType) {
+        this.componentType = componentType;
+    }
+
+    public double getTaxRate() {
+        return taxRate;
+    }
+
+    public void setTaxRate(double taxRate) {
+        this.taxRate = taxRate;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
 }
